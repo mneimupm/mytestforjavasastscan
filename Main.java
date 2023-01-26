42d8391409b40c275d251971f7b40950202c1f48
testusermnupm <123468542+testusermnupm@users.noreply.github.com>
2023-01-26 13:36:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password123";
 
+    public void login() {
+        String inputUsername = "admin";
+        String inputPassword = "password123";
+        if (inputUsername.equals(USERNAME) && inputPassword.equals(PASSWORD)) {
+            System.out.println("Access granted.");
+        } else {
+            System.out.println("Access denied.");
+        }
+    }
+
+    public void vulnerableSQL(String userId) {
+        String query = "SELECT * FROM users WHERE id = " + userId;
+        // execute the query
+    }
+
+    public boolean compareValues(int value1, int value2) {
+        return value1 == value2;
+    }
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
