5d6388eeab397209aacb8a2f6043a9779aa17641
testusermnupm <123468542+testusermnupm@users.noreply.github.com>
2023-01-26 13:58:42 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.*;
 import java.lang.reflect.Field;
+import java.io.*;
 
 public class Main {
 private static final String USERNAME = "admin";
@@ -38,7 +39,30 @@ private static final String USERNAME = "admin";
   }
   throw new NoSuchFieldError(name);
 }
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+	  long data;
+
+			BufferedReader readerBuffered = new BufferedReader(
+					new InputStreamReader(System.in, "UTF-8"));
+			String stringNumber = readerBuffered.readLine();
+			if (stringNumber != null) {
+				data = Long.parseLong(stringNumber.trim());
+			} else {
+				data = 0;
+			}
+
+			// AVOID: potential truncation if input data is very large,
+			// for example 'Long.MAX_VALUE'
+			int scaled = (int)data;
+
+			//...
+
+			// GOOD: use a guard to ensure no truncation occurs
+			int scaled2;
+			if (data > Integer.MIN_VALUE && data < Integer.MAX_VALUE)
+				scaled2 = (int)data;
+			else
+				throw new IllegalArgumentException("Invalid input");
 	  // BAD: A new 'Random' object is created every time
 	// a pseudo-random integer is required.
 	int notReallyRandom = new Random().nextInt();
